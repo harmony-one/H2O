54de446ffd094a3c55b08b60ec651c53508afaee
john-harmony <john@harmony.one>
2019-10-20 21:49:10 -0700

    Use environment variables

--- truffle-config.js
+++ truffle-config.js
@@ -1,64 +1,68 @@
+require('dotenv').config()
 const { TruffleProvider } = require('@harmony-js/core')
-
-
-//one18t4yj4fuutj83uwqckkvxp9gfa0568uc48ggj7
-//0x3aea49553Ce2E478f1c0c5ACC304a84F5F4d1f98
-//01F903CE0C960FF3A9E68E80FF5FFC344358D80CE1C221C3F9711AF07F83A3BD
-
-/*
-
-const phrase =
-  'food response winner warfare indicate visual hundred toilet jealous okay relief tornado'
-const privateKey =
-  '27978f895b11d9c737e1ab1623fde722c04b4f9ccb4ab776bf15932cc72d7c66'
-
-  */
-const phrase = 'urge clog right example dish drill card maximum mix bachelor section select' 
-const private_key = '01F903CE0C960FF3A9E68E80FF5FFC344358D80CE1C221C3F9711AF07F83A3BD'
-const url = 'http://localhost:9500'
-
-//one18t4yj4fuutj83uwqckkvxp9gfa0568uc48ggj7
-const beta_phrase = 'urge clog right example dish drill card maximum mix bachelor section select' 
-//const beta_url = 'https://api.s0.b.hmny.io'
-const beta_url = 'http://34.238.42.51:9500'
-const beta_private_key = '01F903CE0C960FF3A9E68E80FF5FFC344358D80CE1C221C3F9711AF07F83A3BD'
-
-
+const url = process.env.LOCAL_URL;
+const account_1_mnemonic = process.env.MNEMONIC
+const account_1_private_key = process.env.PRIVATE_KEY
+const testnet_url = process.env.TESTNET_URL
+const testnet_0_url = process.env.TESTNET_0_URL
+const testnet_1_url = process.env.TESTNET_1_URL
+gasLimit = process.env.GAS_LIMIT
+gasPrice = process.env.GAS_PRICE
 
 module.exports = {
 
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     development: {
       network_id: '2', // Any network (default: none)
       provider: () => {
         const truffleProvider = new TruffleProvider(
           url,
-          { memonic: phrase },
+          { memonic: account_1_mnemonic },
           { shardID: 0, chainId: 2 },
-          { gasLimit: '936475', gasPrice: '1000000000' },
+          { gasLimit: gasLimit, gasPrice: gasPrice},
         );
-        const newAcc = truffleProvider.addByPrivateKey(private_key);
+        const newAcc = truffleProvider.addByPrivateKey(account_1_private_key);
         truffleProvider.setSigner(newAcc);
         return truffleProvider;
       },
     },
     testnet: {
-      // host: 'localhost', // Localhost (default: none)
-      // port: 9500, // Standard Ethereum port (default: none)
       network_id: '2', // Any network (default: none)
       provider: () => {
         const truffleProvider = new TruffleProvider(
-          beta_url,
+          testnet_url,
+          { memonic: account_1_mnemonic },
+          { shardID: 0, chainId: 2 },
+          { gasLimit: gasLimit, gasPrice: gasPrice},
+        );
+        const newAcc = truffleProvider.addByPrivateKey(account_1_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
+    },
+    testnet0: {
+      network_id: '2', // Any network (default: none)
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          testnet0_url,
           { memonic: beta_phrase },
           { shardID: 0, chainId: 2 },
-          { gasLimit: '6721900', gasPrice: '1000000000' },
+          { gasLimit: gasLimit, gasPrice: gasPrice },
+        );
+        const newAcc = truffleProvider.addByPrivateKey(beta_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
+    },
+    testnet1: {
+      network_id: '2', // Any network (default: none)
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          testnet1_url,
+          { memonic: beta_phrase },
+          { shardID: 1, chainId: 2 },
+          { gasLimit: gasLimit, gasPrice: gasPrice },
         );
         const newAcc = truffleProvider.addByPrivateKey(beta_private_key);
         truffleProvider.setSigner(newAcc);
@@ -75,16 +79,6 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: '0.5.1', // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: false, // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {
-      //   // See the solidity docs for advice about optimization and evmVersion
-      //   optimizer: {
-      //     enabled: false,
-      //     runs: 200,
-      //   },
-      //   evmVersion: 'homestead',
-      // },
     }
   }
 }

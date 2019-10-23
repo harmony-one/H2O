5c694f664fb44fdb235ceee06fc171457521344e
john-harmony <john@harmony.one>
2019-10-23 12:49:46 -0700

    Add in scaffolding for mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -3,9 +3,11 @@ const { TruffleProvider } = require('@harmony-js/core')
 const url = process.env.LOCAL_URL;
 const account_1_mnemonic = process.env.MNEMONIC
 const account_1_private_key = process.env.PRIVATE_KEY
+const mainnet_private_key = process.env.MAINNET_PRIVATE_KEY
 const testnet_url = process.env.TESTNET_URL
 const testnet_0_url = process.env.TESTNET_0_URL
 const testnet_1_url = process.env.TESTNET_1_URL
+const mainnet_0_url = process.env.MAINNET_0_URL
 gasLimit = process.env.GAS_LIMIT
 gasPrice = process.env.GAS_PRICE
 
@@ -65,6 +67,20 @@ module.exports = {
           { gasLimit: gasLimit, gasPrice: gasPrice },
         );
         const newAcc = truffleProvider.addByPrivateKey(account_1_private_key);
+        truffleProvider.setSigner(newAcc);
+        return truffleProvider;
+      },
+    },
+    mainnet0: {
+      network_id: '1', // Any network (default: none)
+      provider: () => {
+        const truffleProvider = new TruffleProvider(
+          mainnet_0_url,
+          { memonic: account_1_mnemonic },
+          { shardID: 0, chainId: 1 },
+          { gasLimit: gasLimit, gasPrice: gasPrice },
+        );
+        const newAcc = truffleProvider.addByPrivateKey(mainnet_private_key);
         truffleProvider.setSigner(newAcc);
         return truffleProvider;
       },
